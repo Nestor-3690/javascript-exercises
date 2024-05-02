6c53551f41d4bc2a2d175fc1342f6cedc499e6c3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-02 17:42:48 +0200

    Add my solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const values = [];
+    let counter = 0;
+    for (let i = 1; i < arguments.length; i++) {
+        values[counter] = arguments[i];
+        counter++;
+    }
+    for (value of values) {
+        for (let i = 0; i < array.length; i++) {
+            if (value === array[i]) {
+                const index = array.indexOf(array[i]);
+                array.splice(index, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 
 };
 
