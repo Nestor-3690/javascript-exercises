05374675c046cab5fffd0aebb1994b6cac8d6fa8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-02 18:08:14 +0200

    Add my solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    if (numberOne < 0 || numberTwo < 0 || typeof numberOne !== "number" || typeof numberTwo !== "number") return "ERROR";
+    let sum = 0;
+    let biggestNumber, lowestNumber;
+    if (numberOne < numberTwo) {
+        biggestNumber = numberTwo;
+        lowestNumber = numberOne
+    } else {
+        biggestNumber = numberOne;
+        lowestNumber = numberTwo;
+    }
+    for (let i = lowestNumber; i <= biggestNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
