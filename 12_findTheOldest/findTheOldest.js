d136a6bb890da5686e979c3f11742f6400d99c29
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-09 14:28:16 +0200

    Add my solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldest = 0;
+    let index = -1;
+    const newArray = array.map((item, itemIndex) => {
+        if (item.yearOfDeath) {
+            if (item.yearOfDeath - item.yearOfBirth > oldest) {
+                oldest = item.yearOfDeath - item.yearOfBirth;
+                index = itemIndex;
+            }
+        } else {
+            item.yearOfDeath = new Date().getFullYear();
+            if (item.yearOfDeath - item.yearOfBirth > oldest) {
+                oldest = item.yearOfDeath - item.yearOfBirth;
+                index = itemIndex;
+            }
+        } return item;
+    })
+    return newArray[index];
 };
 
 // Do not edit below this line
