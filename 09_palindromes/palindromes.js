a4090e57a25e86e4c8285705ed5ba32e60e6702e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-09 11:36:30 +0200

    Add my solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.split('');
+    
+    let lettersArray = []
+    array.forEach((char) => {
+        if (char.match(/^[a-zA-Z0-9]+$/) !== null) {
+            lettersArray.push(char);
+        }
+    })
+    lettersArray.reverse();
 
+    let getLettersString = string.split('');
+    let lettersString = []
+    getLettersString.forEach((char) => {
+        if (char.match(/^[a-zA-Z0-9]+$/) !== null) {
+            lettersString.push(char);
+        }
+    })
+
+    if (lettersString.join('').toLowerCase() === lettersArray.join('').toLowerCase()) {
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
