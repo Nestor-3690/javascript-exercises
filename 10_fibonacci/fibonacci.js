baba130c245a6198890a14576a524ce0aa2e2db5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-09 12:01:07 +0200

    Add my solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    let fibonacci = [0, 1, 1];
+    for (let i = 3; i <= number; i++) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    };
+    return fibonacci[number];
 
 };
 
