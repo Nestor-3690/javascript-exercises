196550c98e1befaba7ca3cbccdbf249fe4571169
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-09 10:48:34 +0200

    Add my solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(...numbers) {
+	let array = [];
+  for (number of numbers) {
+    array.push(number);
+  }
+  return array.reduce((result, item) => result + item, 0);
+};
+
+const subtract = function(...numbers) {
+  let array = [];
+  for (number of numbers) {
+    array.push(number);
+  }
+  return array.reduce((result, item) => result - item);
 	
 };
 
-const subtract = function() {
+const sum = function(numbers) {
+  return numbers.reduce((result, item) => result + item, 0);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(numbers) {
+  return numbers.reduce((result, item) => result * item);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+  if (number === 1) {
+    return 1;
+  } return number * factorial(number - 1);
 };
 
 // Do not edit below this line
